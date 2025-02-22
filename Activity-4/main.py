76131c3d507a56ba401010146a2c702885d39c36
TomiwaMartins21 <martinstomiwa838@gmail.com>
2025-02-22 12:49:53 +0100

    Initial commit

---
+++ Activity-4/main.py
@@ -0,0 +1,15 @@
+import datetime
+
+# using now() to get current
+time
+current_time =
+datetime.datetime.now()
+
+# Print value of now 
+print ("Time now at greenwich")
+maridian is : ", end = "")
+print (current_time)
+
+# print calender of year 2021
+import calendar
+print("\n", calendar.calendar(2021))
